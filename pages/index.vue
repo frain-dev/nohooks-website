5beaca05ba1d58749749ebe9e058dd468559603a
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2023-06-25 13:54:14 +0100

    fix vieo bg issue

--- pages/index.vue
+++ pages/index.vue
@@ -41,7 +41,7 @@
                 <NuxtLink to="https://getconvoy.io/cloud" class="text-14 text-gray-500 font-normal">Powered by <span class="text-primary-400">Convoy</span></NuxtLink>
             </section>
 
-            <section class="px-20px max-w-[988px] w-full mx-auto bg-[url(~/assets/img/video-screenshot.png)] bg-contain bg-center bg-no-repeat">
+            <section class="px-20px max-w-[988px] w-full mx-auto bg-[url(none)] md:bg-[url(~/assets/img/video-screenshot.png)] bg-contain bg-center bg-no-repeat">
                 <video loop autoplay class="rounded-24px shadow-sm border border-primary-50 w-full min-h-[500px]">
                     <source src="~/assets/videos/demo.mp4" type="video/mp4" />
                     <p>
