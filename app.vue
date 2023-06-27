73e6f238f3b68ab2ca8f10e31d7998f3b234457f
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2023-06-27 22:01:33 +0100

    update site seo details

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
 </template>
+
+
+
+<script setup lang="ts">
+useSeoMeta({
+    title: 'Nohooks | Webhooks for platforms with no webhooks',
+    ogTitle: 'Nohooks | Webhooks for platforms with no webhooks',
+    twitterTitle: 'Nohooks | Webhooks for platforms with no webhooks',
+    description: 'Nohooks is a service that offers webhooks functionality on platforms without native webhooks support. Developers can receive real-time notifications of events from supported apps. Powered by Convoy.',
+    ogDescription: 'Nohooks is a service that offers webhooks functionality on platforms without native webhooks support. Developers can receive real-time notifications of events from supported apps. Powered by Convoy.',
+    twitterDescription: 'Nohooks is a service that offers webhooks functionality on platforms without native webhooks support. Developers can receive real-time notifications of events from supported apps. Powered by Convoy.',
+    ogImage: 'https://nohooks.io/seo-img/og-image.png',
+    twitterImage: 'https://nohooks.io/seo-img/og-image.png',
+    twitterCard: 'summary_large_image',
+    twitterImageAlt: 'Nohooks description image',
+    applicationName: 'Nohooks',
+    twitterSite: '@getconvoy',
+    twitterCreator: '@getconvoy',
+    author: 'Convoy',
+    creator: 'Convoy',
+    ogUrl: 'https://nohooks.io'
+})
+</script>
